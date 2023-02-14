8ee4e44e76aa843315e340e796b329dae49f66c9
Sayan <sayanbanerjee2002@gmail.com>
2023-02-14 11:02:21 +0530

    Redux and react-router

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div class="text-center text-red-500 font-bold underline underline-offset-4 text-xl bg-gray-300 h-screen">
+        Welcome to React JS, Tailwind, React Icons,Material UI
+      </div>
+    </>
+  );
+};
+
+export default App;
